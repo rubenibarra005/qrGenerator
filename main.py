4bf960e7ae246e5e22e1db22027fb04573a962d5
rubenibarra0005 <198497967+rubenibarra005@users.noreply.github.com>
2025-12-18 13:59:32 -0600

    Cambios para generar códigos QR en Python con color

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import qrcode
 
-def codigoGenerator(url, nombre):
-    img = qrcode.make(url)
+def codigoGenerator(url, nombre, color_fondo="white", color_code= "black"):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color=color_code, back_color=color_fondo)
     img.save(f"{nombre}.png")
 
 if __name__ == "__main__":
     url = input("Ingresa la url: ")
     nombre  = input("Ingresa el nombre deseado del qr (sin extensión): ")
-    codigoGenerator(url, nombre)
-
+    colores = input("¿Deseas personalizar los colores? (s/n): ").lower()
+    if colores == 's':
+        color_fondo = input("Ingresa el color de fondo (nombre o código hex): ")
+        color_code = input("Ingresa el color del código QR (nombre o código hex): ")
+        codigoGenerator(url, nombre, color_fondo, color_code)
+    else:
+        codigoGenerator(url, nombre)
 
